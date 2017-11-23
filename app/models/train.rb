0bd1e2342b23fb520462e1c90cc6c9263a2f672b
escray <zescray@gmail.com>
2017-11-24 05:42:18 +0800

    7-6 add Train Logo pic

--- app/models/train.rb
+++ app/models/train.rb
@@ -3,6 +3,8 @@ class Train < ApplicationRecord
   validates_presence_of :number
   has_many :reservations
 
+  mount_uploader :train_logo, TrainLogoUploader
+
   def available_seats
     # TODO: 回传有空的座位
     %w[1A 1B 1C 1D 1F]
